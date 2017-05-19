4ddd76b7b6241304ddabc1279aa1243c5f15422a
Xingxian Shou <Xingxian Shou>
2017-05-19 09:14:40 -0400

    second commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,32 +3,59 @@ var questions = ["Which major sports team you could not find in Cleveland?",
                    "How many titles have the Cavaliers have won?",
                    "Which team selected LeBron James as the top pick in the NBA draft?",
                    "What is the most recent year did Indians play the world-series?",
-                   "Which # of jersey dis big-Z wear for Cavaliers?"
+                   "Which # of jersey dis big-Z wear for Cavaliers?",
+                   "How many No. 1 overall picks did Browns get since 1999?",
+                   "How many first round picks did Browns have for 2017 NFL draft?"
                    ];
 var answerChoice = [["Basketball", "Hockey", "Baseball", "Football"],
                     ["2000", "2001", "2002", "2003"],
                     ["none", "1", "2", "3"],
                     ["Bulls", "Celtics", "Cavaliers", "Heat"],
                     ["1997", "2003","2008", "2016"],
-                    ["0", "7", "11", "22"]
+                    ["0", "7", "11", "22"],
+                    ["0", "1", "2", "3"],
+                    ["1","2","3", "4"]
                     ];
 
-var answer = ["Hockey", "2003", "1", "Cavaliers", "2016", "11"];
+var answer = ["Hockey", "2003", "1", "Cavaliers", "2016", "11", "2", "3"];
 
+window.onload = function() {
+
+  //  Click events are done for us:
+//  $(".choice-1").click();
+  //$(".choice-2").click();
+  //$(".choice-3").click();
+  //$(".choice-4").click();
+};
 
 
 
 $(document).ready(function(){
 
-  // display the question and choices to the screen
-
   var pickedQ = [];
   var correctAnswer;
   var randPick;
   var totalQuestions = questions.length;
+  var correctCount = 0;
+  var inCorrectCount = 0;
+
+  // display the question and choices to the screen
+
+    var start = function(){
+      var startBtn = $("<button>");
+          startBtn
+          .addClass("start")
+          .text("START!")
+          .appendTo($(".question"));
+    pickedQ = [];
+    correctCount = 0;
+    inCorrectCount = 0;
+
+  }
 
   var assignQuestion = function(question, answer){
 
+    $(".start").remove();
     $(".question").append(question);
     var answerTemp = $("<button>");
     answerTemp.addClass("answer").text(answer[0]).appendTo($(".choice-1"));
@@ -43,15 +70,27 @@ $(document).ready(function(){
 
   var pickQuestion = function(){
     randPick = Math.floor(Math.random()*totalQuestions);
+    while (pickedQ.indexOf[randPick] > -1){
+      pickedQ.push(randPick);
+    }  
+  
     pickedQ.push(randPick);
     console.log(pickedQ);
     correctAnswer = answerChoice[randPick].indexOf(answer[randPick]);
     console.log(answerChoice[randPick]);
     console.log(correctAnswer);
   }
-  
-  pickQuestion();
-  assignQuestion(questions[randPick], answerChoice[randPick]);
+  start(); 
+  $(".start").on("click", function(){
+    console.log("123");
+    pickQuestion();
+    assignQuestion(questions[randPick], answerChoice[randPick]);
+  })
+
+
+
+  //pickQuestion();
+  //assignQuestion(questions[randPick], answerChoice[randPick]);
   
 
   //initilize the question & answers
@@ -61,12 +100,14 @@ $(document).ready(function(){
       console.log($(this).text());
 
       if(correctAnswer === 0){
-        alert($(this).text());
+        alert("You're correct! Congratulations!");
+        correctCount++;
 
       }
       else
       {
-        alert("wrong!");
+        alert("You picked the wrong option!");
+        inCorrectCount++;
       }
 
     });
